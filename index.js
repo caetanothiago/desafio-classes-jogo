e225a658588bbaa4c0ae649eda3e37c7c10b147c
caetanothiago <caetano.thiago@outlook.com.br>
2024-06-26 06:22:01 -0400

    Projeto Concluido

--- index.js
+++ index.js
@@ -1,10 +1,64 @@
 //Desafio Classes de um Jogo
 
-class hero{
-    constructor (name, age, type){
-        this.name = name
-        this.age = age
-        this.type = type
+var profile = 
+{
+    name: "Thiago",
+    age: 29,
+    type: "ninja",
+}
+
+createProfile (profile)
+
+function createProfile (profile)
+{
+    console.log (`Name: ${profile.name}`)
+    console.log (`Age: ${profile.age}`)
+    console.log (`Type: ${profile.type}`)
+}
+
+class hero
+{
+    constructor (guerreiro, mago, monge, ninja)
+    {
+        this.guerreiro = guerreiro
+        this.mago = mago
+        this.monge = monge
+        this.ninja = ninja
+    }
+    toAttack (attacker)
+    {
+        var attack
+
+        switch (attacker)
+        {
+            case "guerreiro":
+            attack = "espada"
+            console.log (`O ${attacker} atacou usando ${attack}`)
+            break
+
+            case "mago":
+            attack = "magia"
+            console.log (`O ${attacker} atacou usando ${attack}`)
+            break
+
+            case "monge":
+            attack = "artes maciais"
+            console.log (`O ${attacker} atacou usando ${attack}`)
+            break
+
+            case "ninja":
+            attack = "shuriken"
+            console.log (`O ${attacker} atacou usando ${attack}`)
+            break
+            
+            default:
+            console.log ("Classe de Herói escolhida inválida!")
+        }
     }
 }
 
+let typeHero = new hero ("guerreiro", "mago", "monge", "ninja")
+
+let attacker = profile.type
+
+typeHero.toAttack (attacker)
